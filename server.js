f6b39fb29d06d08da22864d96cff6ac1df4fbf63
Vaibhav8208 <vaibhavsd8208@gmail.com>
2025-11-09 19:04:41 +0530

    Temporarily open CORS for debugging

--- server.js
+++ server.js
@@ -32,22 +32,27 @@ const allowedOrigins = [
   "https://homex.net.in"
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"]
+//   })
+// );
 
-// This middleware is essential for reading the request body in POST/PUT requests
+// // This middleware is essential for reading the request body in POST/PUT requests
+// app.use(express.json());
+
+app.use(cors());
+
+
 app.use(express.json());
 
 // Connect to the database
